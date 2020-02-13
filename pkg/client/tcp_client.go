225e8bdd618c315bec8e13d62a07fc4f87adbed6
Julen Dixneuf <julen@student.ecp.fr>
2020-02-13 15:10:16 +0100

    add send name to the client

--- pkg/client/tcp_client.go
+++ pkg/client/tcp_client.go
@@ -3,22 +3,64 @@ package client
 import (
 	"fmt"
 	"net"
+	"strconv"
 )
 
+// A good ressource
+// https://ipfs.io/ipfs/QmfYeDhGH9bZzihBUDEQbCbTc5k5FZKURMUoUvfmc27BwL/socket/tcp_sockets.html
+
+// About net.Conn vs net.TCPConn :
+// > This interface has primary methods ReadFrom and WriteTo to handle packet reads and writes.
+// > The Go net package recommends using these interface types rather than the concrete ones.
+// > But by using them, you lose specific methods such as SetKeepAlive or TCPConn and SetReadBuffer of UDPConn, unless you do a type cast. It is your choice.
+
 // TCPClient connects to the game server
+
+type ClientMsg int
+
+const (
+	NME ClientMsg = iota
+	MOV
+)
+
+type ServerMsg int
+
+const (
+	SET ServerMsg = iota
+	HUM
+	HME
+	MAP
+	UPD
+	END
+	BYE
+)
+
 type TCPClient struct {
-	conn *net.Conn
+	conn net.Conn
 }
 
 // NewTCPClient creates a new TCP client
-func NewTCPClient(ipAddr net.IP, port string) (*TCPClient, error) {
+func NewTCPClient(addr string) (TCPClient, error) {
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", ipAddr, port))
+	// We might need to use net.DialTCP with https://golang.org/pkg/net/#TCPConn.SetKeepAlive
+	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		return nil, err
+		return TCPClient{}, err
 	}
 
-	return &TCPClient{
+	return TCPClient{
 		conn: &conn,
 	}, nil
+}
+
+// SendName to the server
+func (c TCPClient) SendName(name string) error {
+	ASCIIName := strconv.QuoteToASCII(name)
+	t := len(ASCIIName)
+	if t == 0 || t > 127 {
+		return fmt.Errorf("invalid name '%s', please use a short ASCII name", name)
+	}
+	_, err := c.conn.Write([]byte(fmt.Sprintf("NME%d%s", t, ASCIIName)))
+
+	return err
 }
