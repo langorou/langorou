fa2e9e7dd2a8b4ac9f69dce06595ff67ae551c75
Julen Dixneuf <julen@student.ecp.fr>
2020-02-13 14:29:38 +0100

    doing some useless cli stuff

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
 
 	"github.com/langorou/langorou/pkg/client"
 )
@@ -12,14 +14,27 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 	if len(args) < 2 {
-		fmt.Println("please provide IP address and port")
+		fmt.Printf("please provide IP address and port\n")
 		os.Exit(1)
 	}
-	ipAddr := args[0]
-	port := args[1]
-	fmt.Printf("ip %s port %s\n", ipAddr, port)
 
-	_, err := client.NewTcpClient(ipAddr, port)
+	ip := net.ParseIP(args[0])
+	if ip == nil {
+		fmt.Printf("invalid IP address: %s\n", args[0])
+		os.Exit(1)
+	}
+
+	_, err := strconv.ParseUint(args[1], 10, 16) // 0 <= port <= 65535
+	if err != nil {
+		fmt.Printf("invalid port %s, should be between 0 and 65535\n", args[1])
+		os.Exit(1)
+	}
+
+	addr := net.JoinHostPort(ip.String(), args[1])
+	fmt.Printf("connecting to %s\n", addr)
+
+	os.Exit(0)
+	_, err = client.NewTCPClient(ip, args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- pkg/client/tcp_client.go
+++ pkg/client/tcp_client.go
@@ -5,24 +5,20 @@ import (
 	"net"
 )
 
-type TcpClient struct {
+// TCPClient connects to the game server
+type TCPClient struct {
 	conn *net.Conn
 }
 
-// NewTcpClient creates a new TCP client
-func NewTcpClient(ipAddr string, port string) (*TcpClient, error) {
-
-	ip := net.ParseIP(ipAddr)
-	if ip == nil {
-		return nil, fmt.Errorf("invalid IP address: %s", ipAddr)
-	}
+// NewTCPClient creates a new TCP client
+func NewTCPClient(ipAddr net.IP, port string) (*TCPClient, error) {
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", ipAddr, port))
 	if err != nil {
 		return nil, err
 	}
 
-	return &TcpClient{
+	return &TCPClient{
 		conn: &conn,
 	}, nil
 }
