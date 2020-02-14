896192116bf4829cf0c7ea592265aa6a0beb96cc
Victor Paltz <paltz.victor@free.fr>
2020-02-14 11:35:40 +0100

    first implementation of game methods

--- pkg/client/game.go
+++ pkg/client/game.go
@@ -1,8 +1,11 @@
 package client
 
+import "log"
+
 // Game implements the Client interface using a TCP server
 type Game struct {
 	state
+	playerName string
 }
 
 // NewGame creates a new TCP client
@@ -10,16 +13,22 @@ func NewGame() (*Game, error) {
 	return &Game{}, nil
 }
 
-func (T Game) Nme() string {
-	panic("implement me")
+// Nme defines the player name
+func (g *Game) Nme(playerName string) string {
+	g.playerName = playerName
+	return playerName
 }
 
 func (T Game) Mov() []Move {
 	panic("implement me")
 }
 
-func (T Game) Set(n uint8, m uint8) error {
-	panic("implement me")
+// Set initialize an empty grid in the state
+func (g *Game) Set(n uint8, m uint8) {
+	g.state.grid = make([][]cell, n)
+	for i := range g.state.grid {
+		g.state.grid[i] = make([]cell, m)
+	}
 }
 
 func (g *Game) Hum(n uint8, coords []Coordinates) {
@@ -34,12 +43,31 @@ func (T Game) Hme(x uint8, y uint8) error {
 	panic("implement me")
 }
 
-func (T Game) Upd(n uint8, changes []Changes) error {
-	panic("implement me")
+//Upd updates the state of the game
+func (g *Game) Upd(changes []Changes) {
+
+	for _, cha := range changes {
+		if cha.Humans > 0 && cha.Vampires == 0 && cha.Werewolves == 0 {
+			g.state.grid[cha.Y][cha.X].count = cha.Humans
+			g.state.grid[cha.Y][cha.X].race = Villager
+		} else if cha.Humans == 0 && cha.Vampires > 0 && cha.Werewolves == 0 {
+			g.state.grid[cha.Y][cha.X].count = cha.Vampires
+			g.state.grid[cha.Y][cha.X].race = Vampire
+		} else if cha.Humans == 0 && cha.Vampires == 0 && cha.Werewolves > 0 {
+			g.state.grid[cha.Y][cha.X].count = cha.Werewolves
+			g.state.grid[cha.Y][cha.X].race = Werewolf
+		} else if cha.Humans == 0 && cha.Vampires == 0 && cha.Werewolves == 0 {
+			g.state.grid[cha.Y][cha.X].count = 0
+			g.state.grid[cha.Y][cha.X].race = Empty
+		} else {
+			log.Printf("impossible case: only one race on a cell")
+		}
+	}
 }
 
-func (T Game) Map(n uint8, changes []Changes) error {
-	panic("implement me")
+//Map is the same as Upd but is called only once at the beginning
+func (g *Game) Map(changes []Changes) {
+	g.Upd(changes)
 }
 
 func (T Game) End() error {
