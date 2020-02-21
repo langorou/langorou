bdf418133f3caa12f94cafe15f690a5b43450e06
Sami Tabet <salph.tabet@gmail.com>
2020-02-21 12:13:20 +0100

    fmt

--- pkg/client/min_max.go
+++ pkg/client/min_max.go
@@ -1,64 +1,68 @@
 package client
 
-// negamax_alpha returns a score and a turn
-func negamax_alpha (state state, strategy string, alpha float32, player uint8, max_rec uint8) ([]Move, float32){
-	var best_turn []Move  
-	var max_score int = -1000000
-	if max_rec == 0 {
-		potstate = PotentialState {s: state, prob:1}
-		score = scoreState(potstate)
-		return (best_turn, score)
+// negamaxAlpha returns a score and a turn
+func negamaxAlpha(state state, strategy string, alpha float64, player uint8, maxRec uint8) ([]Move, float64) {
+	bestTurn := []Move{}
+	maxScore := -1000000.0
+
+	if maxRec == 0 {
+		potentialState := PotentialState{s: state, prob: 1}
+		return bestTurn, scoreState(potentialState)
 	}
-	for move_list in turn_list(state, strategy, player){
+
+	for _, moveList := range getMoves(state, strategy, player) {
 		//playturn is a better name for evaluateOutcome, it is
-		potstate_list = play_turn(state, player, move_list)
-		
-		score = 0
-		for potstate in potstate_list {
-			_, temp_score = negamax_alpha(potstate.s, strategy, max_score, 1- player, max_rec - 1 )
-			score += temp_score * potstate.prob
+		potentialState := applyMoves(state, player, moveList)
+
+		score := 0.0
+		for _, potentialState := range potentialState {
+			_, tmpScore := negamaxAlpha(potentialState.s, strategy, maxScore, 1-player, maxRec-1)
+			score += tmpScore * potentialState.prob
 		}
-		score = -score 
-		if score> max_score{
-			max_score = score
-			best_turn = move_list
+		score = -score
+		if score > maxScore {
+			maxScore = score
+			bestTurn = moveList
 		}
-		//todo save previously modified cells by the play_turn function
-		//we use a deep copy for now 
-		//reverse_play_turn(state, player, move_list) dans le cas 
+		//todo save previously modified cells by the applyMoves function
+		//we use a deep copy for now
+		//undoMoves(state, player, moveList) dans le cas
 		//ou pas de bataille aleatoire pour eviter les copies
-		
-		if max_score > -alpha{
+
+		if maxScore > -alpha {
 			break
 		}
-		
+
 	}
-	return best_turn, max_score
-	
+	return bestTurn, maxScore
+
 }
 
+// getMoves returns a list of moves
+func getMoves(state state, strategy string, player uint8) [][]Move {
+	max := 10
+	movesMat := make([][]Move, max)
 
+	for i := range movesMat {
+		movesMat[i] = []Move{}
 
-
-func turn_list (state state, strategy string, player uint8) [][]Move {
-	//renvoie une liste de de liste de move 
-	for cell in 	
-	var nb_move int = 10
-	var turn_list [][]Move = make([][]Move, 0)
-	for i:=0; i<nb_move; i++ {
-		turn_list[i]  = make([]Move, 0) 
-		for j:= 0; j<nb_move; j++ {
-			turn_list[i][j] = Move{ Start: Coordinates{X:i,Y:j}, N:1, End:Coordinates{X:i,Y:j+1}}
+		for j := 0; j < max; j++ {
+			movesMat[i][j] = Move{
+				Start: Coordinates{X: uint8(i), Y: uint8(j)},
+				N:     1,
+				End:   Coordinates{X: uint8(i), Y: uint8(j + 1)},
+			}
 		}
 	}
+
+	return movesMat
 }
 
-func play_turn(state state, player uint8, move_list []Move) []PotentialState {
-	evaluateMoveOutcome ()
-	for move in move_list
+func applyMoves(state state, player uint8, moveList []Move) []PotentialState {
+	// TODO
+	return nil
 }
 
-func reverse_play_turn(state state, player uint8, move_list []Move){
+func undoMoves(state state, player uint8, moveList []Move) {
 	//TODO
 }
-
