bc2f8ebc3be4963ff9be99c78f5db72d0cbb83c6
Sami Tabet <salph.tabet@gmail.com>
2020-03-15 16:21:55 +0100

    add todo

--- pkg/client/min_max_ia.go
+++ pkg/client/min_max_ia.go
@@ -20,6 +20,7 @@ func NewMinMaxIA(depth uint8) *MinMaxIA {
 }
 
 func (m *MinMaxIA) Play(state model.State) model.Coup {
+	// TODO: use findBestCoupWithTimeout
 	coup, score := m.heuristic.findBestCoup(state, m.depth)
 	log.Printf("MinMaxIA computed a coup with score: %f", score)
 	return coup
